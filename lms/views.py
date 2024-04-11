2150521efcf2534815baef5aa8419b4c220bb918
ElnaraGasanova <elnaraskypro@gmail.com>
2024-04-11 21:23:47 +0300

    Описан CRUD. Для курса Viewsets, а для урока - Generic-классы.

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    '''Описываем ViewSet для работы с моделью.'''
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    '''Описываем контроллеры на основе дженерик.'''
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    '''Описываем контроллеры на основе дженерик.'''
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()

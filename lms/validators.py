51892823cec97095bf2a4e2d46b9ac41c957abad
ElnaraGasanova <elnaraskypro@gmail.com>
2024-05-02 18:37:12 +0300

    Удалила None

--- lms/validators.py
+++ lms/validators.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class URLValidator:
     '''Класс валидации ссылки.'''
-    def __init__(self, field) -> None:
+    def __init__(self, field):
         self.field = field
 
     def __call__(self, value):

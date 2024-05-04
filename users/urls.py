4e11eeb1bd93c0802b94d2c63fe3a9fa12752d8e
ElnaraGasanova <elnaraskypro@gmail.com>
2024-05-04 15:51:47 +0300

    Установлен stripe

--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,13 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentsListAPIView, UserCreateAPIView, UserListAPIView, UserUpdateAPIView, UserDestroyAPIView
+from users.views import PaymentsListAPIView, UserCreateAPIView, UserListAPIView, UserUpdateAPIView, UserDestroyAPIView, \
+    PaymentsCreateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('payments/create/', PaymentsCreateAPIView.as_view(), name='payments_create'),
     path('payments/', PaymentsListAPIView.as_view(), name='payments_list'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token'),
